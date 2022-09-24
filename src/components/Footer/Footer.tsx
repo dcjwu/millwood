38155f54def214f9d1004ac6b6bd22ae1ad5137a
AK <kkucenko.aleksejs@gmail.com>
2022-09-24 21:18:09 +0300

    Feature | Add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,45 @@
 import React from "react"
 
+import { Text } from "@lib/Text/Text"
+
 import s from "./Footer.module.scss"
 
 export const Footer: React.FC = (): JSX.Element => {
    return (
       <div className={s.footer}>
-         FOOTER
+         <div>
+            <Text color="white" component="p" variant="paragraph">
+               &quot;Millhouse Trade&quot; SIA
+            </Text>
+            <Text color="white" component="p" variant="paragraph">
+               Reg Nr: 440203387800
+            </Text>
+            <Text color="white" component="p" variant="paragraph">
+               Riga, Latvia
+            </Text>
+         </div>
+         <div>
+            <Text color="white" component="p" variant="paragraph">
+               &quot;Millhouse Trade&quot; SIA
+            </Text>
+            <Text color="white" component="p" variant="paragraph">
+               Reg Nr: 440203387800
+            </Text>
+            <Text color="white" component="p" variant="paragraph">
+               Riga, Latvia
+            </Text>
+         </div>
+         <div>
+            <Text color="white" component="p" variant="paragraph">
+               &quot;Millhouse Trade&quot; SIA
+            </Text>
+            <Text color="white" component="p" variant="paragraph">
+               Reg Nr: 440203387800
+            </Text>
+            <Text color="white" component="p" variant="paragraph">
+               Riga, Latvia
+            </Text>
+         </div>
       </div>
    )
 }
