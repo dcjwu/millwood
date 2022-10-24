7110d78250e5a6699ce146412562ba41e96a9fab
AK <kkucenko.aleksejs@gmail.com>
2022-10-24 17:36:39 +0300

    Chore | Revert

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 
 import Aos from "aos"
-import axios from "axios"
 import dynamic from "next/dynamic"
 import Script from "next/script"
 
@@ -18,11 +17,7 @@ const Topbar = dynamic(() => import("@components/Topbar/Topbar").then(module => 
 const About = dynamic(() => import("@components/About/About").then(module => module.About))
 const Form = dynamic(() => import("@components/Form/Form").then(module => module.Form))
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-const Home: NextPage = ({ data }) => {
-
-   console.table(data)
+const Home: NextPage = () => {
 
    React.useEffect(() => {
       Aos.init({
@@ -75,11 +70,3 @@ const Home: NextPage = ({ data }) => {
 }
 
 export default Home
-
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const getServerSideProps = async () => {
-   const res = await axios.get("https://pdf-generator-indol.vercel.app/api/users")
-   // const res = await axios.get("http://localhost:3000/api/users")
-   const { data } = res
-   return { props: { data } }
-}
